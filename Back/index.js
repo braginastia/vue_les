b8bdb3b61ed48eefb7ac1e042243c54f5a4d9b11
Artur <103675840+Ar2r906@users.noreply.github.com>
2024-03-05 16:36:40 +0500

    Update index.js

--- Back/index.js
+++ Back/index.js
@@ -7,14 +7,16 @@ app.use(express.urlencoded({ extended: true }))
 
 const port = process.env.PORT
 const auths_routes = require('./routes/auths')
-// const users_routes = require('./routes/')
+// const users_routes = require('./routes/users')
+// const posts_routes = require('./routes/posts')
 
 app.use(cors({
     origin: ['http://localhost:8080']
 }))
 
-app.use('/api/auth', auths_routes)
-// app.use('/api/user', users_routes)
+app.use('/api/auth', auths_routes);
+// app.use('/api/users', users_routes);
+// app.use('/api/posts', posts_routes);
 
 app.use((error, request, response, next) => {
     if(error instanceof SyntaxError)
