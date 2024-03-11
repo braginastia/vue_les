9c765df5b524c84cdcb4399ab5a80788eb27a765
Artur <103675840+Ar2r906@users.noreply.github.com>
2024-03-11 18:11:57 +0500

    Update index.js

--- Back/index.js
+++ Back/index.js
@@ -7,24 +7,19 @@ app.use(express.urlencoded({ extended: true }))
 
 const port = process.env.PORT
 const auths_routes = require('./routes/auths')
-// const users_routes = require('./routes/users')
-// const posts_routes = require('./routes/posts')
 
 app.use(cors({
     origin: ['http://localhost:8080']
 }))
 
 app.use('/api/auth', auths_routes);
-// app.use('/api/users', users_routes);
-// app.use('/api/posts', posts_routes);
 
 app.use((error, request, response, next) => {
     if(error instanceof SyntaxError)
         response.status(404).send({ message: 'not valid data' })
-    else 
-        next()
+    else next()
 })
 
 app.listen(port, ()=>{
-    console.log(`Server started om port ${port}`);
+    console.log(`Server started on port ${port}`);
 })
