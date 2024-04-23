686bbe600699c4c454b9b33da983e63df5ea4b49
Artur <103675840+Ar2r906@users.noreply.github.com>
2024-04-23 21:48:35 +0500

    Update index.js

--- Back/index.js
+++ Back/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 require('dotenv').config()
-let cors = require('cors')
+const cors = require('cors')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
